d47ca90461d412a59f408343223d1710620be966
Subkhon Amrulloev <subkhon.amrulloev@blackrock.com>
2019-03-21 04:43:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function logWhisper(string){
 
 function sayHiToGrandma(string){
   if (string=="I love you, Grandma."){
-    return "I love you too!";
+    return "I love you too.";
   }
   if (string==whisper(string)){
     return "I can't hear you!";
